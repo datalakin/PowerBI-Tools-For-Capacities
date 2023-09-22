c8a7298e8d31f7b7fec389baf92a3eb286cd9cb0
datalakin <144113720+datalakin@users.noreply.github.com>
2023-09-22 09:47:50 +0100

    Update Setup_Load_Test.ps1
    Updated to include new report parameters, and "access capacity" ability

--- RealisticLoadTestTool/Setup_Load_Test.ps1
+++ RealisticLoadTestTool/Setup_Load_Test.ps1
@@ -34,9 +34,11 @@ $pageIDRegex = '\/\/"pageName".*"' # MK find page id parameter
 $bookmarksRegex = '\/\/"bookmarkList".*]' # MK find bookmarks parameter
 $noViewsRegex = '"numberOfViews":\s*(\d+),' # MK find number of views parameter
 $filtersRegex = '"filters".*]' #MK find filters parameter
-$thinkTimeRegex = '"thinkTimeSeconds":\s*(\d+)' # MK find think time parameter
+$thinkTimeRegex = '"thinkTimeSeconds":\s*(\d+),' # MK find think time parameter
 $projectNameRegex = '"projectName":.*"'# MK find project name parameter
 
+$noUsersRegex = '"numberOfUsers":\s*(\d+),' # ML find number of users parameter
+$skuRegex = '"sku":.*"' # MK find sku parameter
 
 #Function implementation to update token file
 function UpdateTokenFile
@@ -55,8 +57,12 @@ function UpdateReportParameters
     $reportJSONFile = Get-Content $(Join-Path $workingDir 'PBIReport.JSON') -raw;
     $new_ReportJSONFile = ($reportJSONFile -replace $reportUrlRegex,$args[0]);
     $new_ReportJSONFile = ($new_ReportJSONFile -replace $noViewsRegex,('"numberOfViews": ' + $args[1] + ',')); # MK set number of views based on user input
-    $new_ReportJSONFile = ($new_ReportJSONFile -replace $thinkTimeRegex,('"thinkTimeSeconds": '+ $args[2])); # MK set think time based on user input
+    $new_ReportJSONFile = ($new_ReportJSONFile -replace $thinkTimeRegex,('"thinkTimeSeconds": '+ $args[2] + ',')); # MK set think time based on user input
     $new_ReportJSONFile = ($new_ReportJSONFile -replace $projectNameRegex,('"projectName": "' + $args[3] + '"')); # MK set think time based on user input
+
+    $new_ReportJSONFile = ($new_ReportJSONFile -replace $noUsersRegex,('"numberOfUsers": ' + $args[4] + ',')); # ML set numbers of users based on user input
+    $new_ReportJSONFile = ($new_ReportJSONFile -replace $skuRegex,('"sku": "' + $args[5] + '"')); # ML set capacity SKU based on workspace capacity
+
     $filtersJSONFile = Get-Content $(Join-Path $workingDir $filtersFileName) -raw; #MK get filters from the filter file
     $pageJSONFile = Get-Content $(Join-Path $workingDir $pageFileName) -raw; #MK get filters from the filter file
     $bookmarksJSONFile = Get-Content $(Join-Path $workingDir $bookmarksFileName) -raw; #MK get filters from the filter file
@@ -85,6 +91,7 @@ while($masterFilesExists)
 
 [int]$reportCount = Read-Host "How many reports you want to configure?"
 [int]$noViews = Read-Host "How many views you want to generate per report?"
+[int]$noUsers = Read-Host "How many users do you want to replicate?"
 [int]$thinkTime = Read-Host "How much think time between interaction? (sec)"
 $projectName = Read-Host "What is the name of the project?"
 $increment = 1
@@ -101,6 +108,7 @@ while($reportCount -gt 0)
     $workSpaceList = Get-PowerBIWorkspace
     #can add my workspace and then not specify the WorkspaceId switch when we list reports below... TODO
 
+
     $workSpaceCounter = 1
     foreach($workSpace in $workSpaceList)
     {
@@ -109,7 +117,30 @@ while($reportCount -gt 0)
         ++$workSpaceCounter
     }
 
-    $workSpaceSelection = Read-Host "Select Work space index from above"
+    $workSpaceSelection = Read-Host "
+    
+    Select Work space index from above"
+
+    #Get the capacity SKU being used
+
+    Write-Host "
+    Retrieving capacity information...
+    "
+    
+    $WorkspaceCapacityId = (Get-PowerBIWorkspace -Id $($workSpaceList[$workSpaceSelection-1].Id)).CapacityId
+    $Capacity = (Get-PowerBICapacity | where Id -eq $WorkspaceCapacityId).DisplayName
+    $CapacitySku = (Get-PowerBICapacity | where Id -eq $WorkspaceCapacityId).Sku
+
+    if ($null -eq $WorkspaceCapacityId)
+    {
+        Write-Host "
+        No capacity detected, or you do not have sufficient access.
+        "
+    }
+    else
+    {
+    Write-Host "Workspace is using Capacity $($Capacity) with SKU $($CapacitySku)"
+    }
 
     #Accessing reports from selected work space
     Write-Host "Listing all reports from the selected work space" -ForegroundColor Yellow
@@ -142,7 +173,7 @@ while($reportCount -gt 0)
     UpdateTokenFile
 
     #Function call to update report parameters file
-    UpdateReportParameters $reportUrl $noViews $thinkTime $projectName
+    UpdateReportParameters $reportUrl $noViews $thinkTime $projectName $noUsers $CapacitySku
     
     $reportConfig.WorkSpace = $($workSpaceList[$workSpaceSelection-1].Name)
     $reportConfig.ReportName = $($reportList[$reportSelection-1].Name)
